c602c9724f716cba4faff6c9876825a25c8b5244
user <mbarbag@unal.edu.co>
2019-03-10 16:39:13 -0500

    Listas funciona

--- fs/main.py
+++ fs/main.py
@@ -1,1 +1,19 @@
 import os
+def list(source):
+    return os.listdir(source)
+
+def removeFile(source):
+    return os.remove(source)
+
+def removeDirs(source):
+    return os.removedirs(source)
+
+def createFile(file_name):
+    file = open(file_name, "w+")
+    return file.close()
+
+def createDirs(source):
+    return os.makedirs(source)
+
+
+print(list('/home/user'))
