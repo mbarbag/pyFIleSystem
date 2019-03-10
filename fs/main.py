563d93378f4e0886e691d93753ee8dba2b2737bc
Augusto <augusto9843@gmail.com>
2019-03-10 17:42:53 -0500

    Esta funcionando listar, borrar, crear

--- fs/main.py
+++ fs/main.py
@@ -8,12 +8,14 @@ def removeFile(source):
 def removeDirs(source):
     return os.removedirs(source)
 
-def createFile(file_name):
-    file = open(file_name, "w+")
+def createFile(source):
+    file = open(source, "w+")
     return file.close()
 
 def createDirs(source):
     return os.makedirs(source)
 
 
-print(list('/home/user'))
+createFile("/home/augusto/Desktop/manuAugus.txt")
+
+print(list('/home/augusto/Desktop'))
